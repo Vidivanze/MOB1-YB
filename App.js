da9ab42837151e65a6899afd00ce8428a889e6b0
Yvann <yvann.butticaz@gmail.com>
2021-05-21 12:13:22 +0200

    Add form and base picker

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
+import { Header, ThemeProvider, Tab} from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Bienvenue !</Text>
-      <StatusBar style="auto" />
-      <Login></Login>
-    </View>
+    <SafeAreaProvider>
+
+      <View style={styles.container}>
+        <Header
+          //leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{ text: 'CSU', style: { color: '#fff' } }}
+          //rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+        
+        <Tab>
+          <Tab.Item title="Recent" />
+          <Tab.Item title="favourite" />
+          <Tab.Item title="cart" />
+        </Tab>
+      </View>
+
+      <View style={styles.content}>
+        <Text>Bienvenue !</Text>
+        <Login></Login>         
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  top: {
+    backgroundColor: '#fffff',
+  },
+
+  content: {
+    marginTop: 30,
+    backgroundColor: '#fffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
 });
--- screens/Login.js
+++ screens/Login.js
@@ -1,33 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import { View, Text, TextInput, StyleSheet} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import { Button , Input} from 'react-native-elements';
+
+import axios from 'axios';
+import API from '../api/Api';   
 
 class Login extends Component {
-    state = {
-        base: '', 
+    constructor(props){
+        super(props);
+        this.state = {
+            bases: new Array()
+        }
     }
     
+    componentDidMount(){
+        API.get('bases')
+        .then(res => {
+            this.setState({bases: res.data});
+            console.log(this.state.bases);
+        })
+    }
+   
+
     render() {
     return (
         <View>
-            <TextInput placeholder="Initiales"/>
-            <TextInput secureTextEntry={true} placeholder="Mot de passe"/>
-
-            <Picker
-                selectedValue={this.currency}>
-                <Picker.Item label="USD" value="US Dollars" />
-                <Picker.Item label="EUR" value="Euro" />
-                <Picker.Item label="NGN" value="Naira" />
+            <View style={{flex:2}}>
+                <Input placeholder="Initiales"/>
+                <Input secureTextEntry={true} placeholder="Mot de passe"/>
+            </View>
+            <View style={{flex:2}}>
+                <Picker style={{marginTop: 10}}>
+                    {this.state.bases.map(base =>
+                        <Picker.Item label={base.name} value={base.id} />    
+                    )}
                 </Picker>
-            <Text>
-            Selected: {this.currency}
-            </Text>
+            </View>
 
-            <Button title="Learn More" color="#841584" accessibilityLabel="Connexion"/>
+            <Button style={{marginTop: 10}} title="Connexion" color="#841584"/>
+            
 
         </View>
     );
+
+
   }
 }
+
+
 
 export default Login;
