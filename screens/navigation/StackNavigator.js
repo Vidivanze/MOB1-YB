c7fffb008d288922b9d5058bcb5b6a516ec0dde7
Yvann <34337152+MrHD586@users.noreply.github.com>
2021-06-18 00:21:58 +0200

    Clean stack navigator

--- screens/navigation/StackNavigator.js
+++ screens/navigation/StackNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Modal} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { UserContext } from '../../context/UserContext';
@@ -15,62 +15,68 @@ import Reports from '../Reports';
 
 const Stack = createStackNavigator();
 
-const screenOptionStyle1 = {
-  headerStyle: {
-    backgroundColor: "#rgb(32, 137, 220)",
-  },
-  headerTintColor: "white",
-  headerBackTitle: "Back",
-};
-
-const screenOptionStyle2 = {
-  headerStyle: {
-    backgroundColor: "#rgb(32, 137, 220)",
-  },
-  headerTintColor: "white",
-  headerBackTitle: "Back",
-};
-
-
+//Login
 export class LoginStackNavigator extends Component{
   render(){
     return (
-      <Stack.Navigator screenOptions={screenOptionStyle1}>
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: "#rgb(32, 137, 220)",
+        },
+        headerTintColor: "white",
+        headerBackTitle: "Back",
+      }}>
         <Stack.Screen name="CSU APP" component={Login} />
       </Stack.Navigator>
     );
   }
 }
 
+//Call the tab navigator home
 export class HomeStackNavigator extends Component{
   render(){ 
     return (<Home></Home>);
   }
 }
 
+//Consultations component is from the tab navigator
 export class ConsultationsStackNavigator extends Component{
   render(){
     return (
-      <Stack.Navigator screenOptions={{     
-        headerStyle: {
-          backgroundColor: "#rgb(32, 137, 220)",
-        },
-        headerTintColor: "white",
-        headerBackTitle: "Back",
-        headerRight: () => (
-          <Text style={style.userInfos}>{this.context.initials}@{this.context.selectedBase.name}  
-            <Ionicons name="log-out-outline" size="30px" onPress={() => this.context.logMeOut()} style={style.logOutIcon}></Ionicons>
-          </Text>
-        ),
-      }}>
+      <DefaultStackNavigator>
         <Stack.Screen name="List" component={Consultations} />
         <Stack.Screen name="Details" component={ConsultationDetails} />
-      </Stack.Navigator>
+      </DefaultStackNavigator>
     );
   }
 }
 
+//Reports component is from the tab navigator
 export class ReportsStackNavigator extends Component{
+  render(){
+    return (
+      <DefaultStackNavigator>
+        <Stack.Screen name="List" component={Reports} />
+      </DefaultStackNavigator>
+    );
+  } 
+}
+
+//Default stack navigator
+export class DefaultStackNavigator extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      modalVisible: false
+    }
+  }
+
+  setModalVisible(state){
+    console.log(this.state.modalVisible)
+    this.setState({modalVisible: state})
+    console.log(this.state.modalVisible)
+  }
+
   render(){
     return (
       <Stack.Navigator screenOptions={{     
@@ -80,18 +86,44 @@ export class ReportsStackNavigator extends Component{
         headerTintColor: "white",
         headerBackTitle: "Back",
         headerRight: () => (
-          <Text style={style.userInfos}>{this.context.initials}@{this.context.selectedBase.name}  
-            <Ionicons name="log-out-outline" size="30px" onPress={() => this.context.logMeOut()} style={style.logOutIcon}></Ionicons>
-          </Text>
+          <View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}
+            >
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <Text style={styles.modalText}>Êtes-vous sur de vouloir vous déconnecter ?</Text>
+                  <View style={{flex:1, flexDirection: 'row'}}>
+                    <Pressable style={[styles.button, styles.buttonLogout]} onPress={() => this.context.logMeOut()}>                 
+                      <Text style={styles.textStyle}>Déconnexion</Text>
+                    </Pressable>
+                    <Pressable style={[styles.button, styles.buttonClose]} onPress={() => this.setModalVisible(!this.state.modalVisible)}>
+                      <Text style={styles.textStyle}>Annuler</Text>
+                    </Pressable>
+                  </View>
+                </View>
+              </View>
+            </Modal>
+
+            <Text style={styles.userInfos}>{this.context.initials}@{this.context.selectedBase.name}   
+              <Ionicons name="log-out-outline" size="30px" onPress={() => this.setModalVisible(!this.state.modalVisible)} style={styles.logOutIcon}></Ionicons>
+            </Text>
+          </View>
         ),
       }}>
-        <Stack.Screen name="List" component={Reports} />
+        {this.props.children}
       </Stack.Navigator>
     );
-  } 
+
+  }
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   logOutIcon: {
     paddingLeft: '15px', 
     paddingRight: '15px',
@@ -101,8 +133,57 @@ const style = StyleSheet.create({
   userInfos: {
       color: "#fff",
       alignContent: 'center'
+  },
+
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+    marginLeft: 10,
+  },
+  buttonLogout: {
+    backgroundColor: "#f04048",
+    marginRight: 10,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
   }
 });
   
 ConsultationsStackNavigator.contextType = UserContext;
 ReportsStackNavigator.contextType = UserContext;
+DefaultStackNavigator.contextType = UserContext;
